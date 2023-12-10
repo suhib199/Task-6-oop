7939cd7300e2ac4cf73d7bad1073bf8aa56ed244
Suhib <alhanafisuhibe@gmail.com>
2023-12-10 15:21:04 +0300

    Add project files.

---
+++ Task 6 oop/Car.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_6_oop
+{
+
+    public class AditionalDetails
+    {
+        public string GearBox;
+        public double Weigh;
+        public string SeatsNumber;
+
+        public AditionalDetails(string gearBox, double weigh, string seatsNumber)
+        {
+            GearBox = gearBox;
+            Weigh = weigh;
+            SeatsNumber = seatsNumber;
+        }
+    }
+
+    internal class Car : AditionalDetails
+    {
+        public DateOnly Year;
+        public string Type;
+        public double Price;
+        public string Model;
+        public string PalletNo;
+        public string Color;
+        public double Tempreture;
+
+        public Car(DateOnly year, string type, double price, string model, string palletNo, string color, double temp, string gearBox, double weigh, string seatsNumber): base(gearBox, weigh, seatsNumber)
+        {
+            Year = year;
+            Type = type;
+            Price = price;
+            Model = model;
+            PalletNo = palletNo;
+            Color = color;
+            Tempreture = temp;
+        }
+
+        public string Engine(double temp)
+        {
+            if (Tempreture < 50)
+            {
+                return "Start Engine";
+            }
+            else
+            {
+                return "Stop Engine";
+            }
+        }
+    }
+}
+  
---
+++ Task 6 oop/Program.cs
@@ -0,0 +1,18 @@
+﻿using Task_6_oop;
+
+
+Car car1 = new Car(new DateOnly(2002, 7, 1), "Toyota", 25000, "Anything", "12465465", "Red", 40.2, "Automatic", 15, "4");
+Console.WriteLine("Car Details:");
+Console.WriteLine($"Year: {car1.Year}");
+Console.WriteLine($"Type: {car1.Type}");
+Console.WriteLine($"Price: {car1.Price}JD");
+Console.WriteLine($"Model: {car1.Model}");
+Console.WriteLine($"Pallet No: {car1.PalletNo}");
+Console.WriteLine($"Color: {car1.Color}");
+Console.WriteLine($"Temperature: {car1.Tempreture}C");
+Console.WriteLine($"GearBox: {car1.GearBox}");
+Console.WriteLine($"Weight: {car1.Weigh} kg");
+Console.WriteLine($"Seats Number: {car1.SeatsNumber}");
+
+
+Console.WriteLine($"Engine Status: {car1.Engine(car1.Tempreture)}");
